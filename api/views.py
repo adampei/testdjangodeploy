ff69f020a3b5c5f5d589e4700f5f3ad9c5229d65
Adampei <rukeypei@gmail.com>
2024-11-28 13:52:50 +0800

    x

--- api/views.py
+++ api/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+import logging
+
+logger = logging.getLogger("我的django日志:")
 
 
 # Create your views here.
 def index(request):
+    logger.debug("debug信息")
+    logger.info("info信息")
+    logger.warning("warning信息")
+    logger.error("error信息")
     return render(request, "index.html")
 
 
